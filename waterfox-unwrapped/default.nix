9631d5c261bf6decabefa21ea10d0dcc7a1409a2
Hythera <87016780+Hythera@users.noreply.github.com>
2026-06-03 19:08:42 +0200

    waterfox-unwrapped: 6.6.13 -> 6.6.14

--- waterfox-unwrapped/default.nix
+++ waterfox-unwrapped/default.nix
@@ -8,7 +8,7 @@
 }:
 buildMozillaMach rec {
   pname = "waterfox";
-  version = "6.6.13";
+  version = "6.6.14";
   applicationName = "Waterfox";
   binaryName = "waterfox";
   branding = "waterfox/browser/branding";
@@ -16,7 +16,7 @@ buildMozillaMach rec {
     owner = "BrowserWorks";
     repo = "Waterfox";
     tag = version;
-    hash = "sha256-fHbEF1C0nwRaEjTpKKDDO2FoyVaxQL0GaskHrCbBEcc=";
+    hash = "sha256-SjN/LotSmTEePS/eECUvJkiPiv5NfnSxME1EULHuhDY=";
     fetchSubmodules = true;
     preFetch = ''
       export GIT_CONFIG_COUNT=1
