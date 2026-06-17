f0ebb2019a8d993cb6bbd3cdc4f36817de8a0698
Hythera <87016780+Hythera@users.noreply.github.com>
2026-06-17 16:43:20 +0200

    flake.nix: modernize, provide default.nix

--- flake.nix
+++ flake.nix
@@ -1,44 +1,20 @@
 {
   description = "Nix implementation of Waterfox";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
     {
-      self,
-      nixpkgs,
-      ...
-    }@inputs:
-    {
-      overlays.default = final: prev: {
-        waterfox = prev.callPackage ./waterfox/default.nix {
-          waterfox-unwrapped = prev.callPackage ./waterfox-unwrapped/default.nix { };
-        };
-        waterfox-bin = prev.callPackage ./waterfox-bin/default.nix {
-          waterfox-bin-unwrapped = prev.callPackage ./waterfox-bin-unwrapped/default.nix { };
-        };
-        waterfox-bin-unwrapped = prev.callPackage ./waterfox-bin-unwrapped/default.nix { };
-        waterfox-unwrapped = prev.callPackage ./waterfox-unwrapped/default.nix { };
-      };
-    }
-    // (inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-      in
-      {
-        packages = {
-          waterfox = pkgs.waterfox;
-          waterfox-bin = pkgs.waterfox-bin;
-          waterfox-bin-unwrapped = pkgs.waterfox-bin-unwrapped;
-          waterfox-unwrapped = pkgs.waterfox-unwrapped;
-        };
-      }
-    ));
+      legacyPackages = forAllSystems (
+        system:
+        import ./default.nix {
+          pkgs = import nixpkgs { inherit system; };
+        }
+      );
+      packages = forAllSystems (
+        system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
+      );
+    };
 }
